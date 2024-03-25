d9b99bfe31fbba4180ddc7fdb53c96c9c8041706
Hana Pavlova <hanapavlova7@gmail.com>
2024-03-25 12:48:00 +0100

    pripraveno

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const displayed = document.querySelector(".display")
+
+const handleDigitClick = (event) => {
+    if (displayed.textContent.length >= 9) {
+        return;
+    }
+    let button = event.target.textContent
+        if (displayed.textContent === '0') {
+        displayed.textContent = button;
+        } else {
+        displayed.textContent += button;
+        }
+}
+
+const keypadButtons = document.querySelectorAll(".keypad")
+keypadButtons.forEach(button => {
+    button.addEventListener("click", handleDigitClick)
+})
 
